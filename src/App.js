87f45dd9c74c8664f21d9149cae299bd34fd4f7a
Oleksandr Krokhmalnyi <oleksandr.kr@aquimarketing.com>
2021-08-12 11:09:23 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [albumId, setAlbumId] = useState(0);
+    const [albums, setAlbums] = useState([]);
+
+    const onClick = (albumId) => (e) => {
+        setAlbumId(albumId);
+    }
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/albums")
+            .then((response) => response.json())
+            .then((data) => {
+                setAlbums(data);
+            });
+    });
+
+    return (
+        <div className="gallery">
+            <aside className="albums">
+                <h4 className="albums__title">Albums</h4>
+                <ul className="albums-list">
+                    {albums.map(({ id, title }) => {
+                        let classes = "albums-list__item";
+
+                        if (albumId === id) {
+                            classes += " albums-list__item--active";
+                        }
+
+                        return (
+                            <li key={id} className={classes} onClick={onClick(id)}>{title}</li>
+                        );
+                    })}
+                </ul>
+            </aside>
+            <main className="photos" />
+        </div>
+    );
 }
 
 export default App;
